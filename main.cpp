68602c03bbed53d5ea795d68c67f2fd9d4ad441d
Juan Cucurell <zarcophage@localhost.localdomain>
2022-04-10 16:29:57 -0400

    embelleciendo la cosa

--- main.cpp
+++ main.cpp
@@ -50,35 +50,6 @@ void lectura_text(){
     file.close();
 };
 
-void translate_binary(){
-    Ticket tickets;
-    int totalTickets;
-    ifstream binaryFile;
-    ofstream textFile;
-    binaryFile.open("casoT1/tickets.dat", ios::binary);
-    if (!binaryFile.is_open()){
-        cout<<"Binary file is damaged or missing"<<endl;
-        return;
-    }; 
-    textFile.open("casoT1/textTickets.txt");
-    if (!textFile.is_open()){
-        cout<<"New file is damaged"<<endl;
-        return;
-    }
-    
-    binaryFile.read((char*) &totalTickets, sizeof(int)); //pointer to memory address where the value will be stored, and byte size of the value
-    textFile << totalTickets << endl;
-    binaryFile.seekg(1*sizeof(int)); //repositioning cursor inside the file
-
-    while (binaryFile.read((char*) &tickets, sizeof(Ticket))){
-        textFile << tickets.rut_funcionario << " " <<tickets.day_of_month<< " " << tickets.time << endl;
-
-    };
-
-    binaryFile.close();
-    textFile.close();
-}
-
 void leer_bin(){
     ifstream file;
     file.open("casoT1/tickets.dat", ios::binary);
