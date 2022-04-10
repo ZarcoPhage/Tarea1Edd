9f10a397d174f2c038f614e6907c34c79d220ff3
Juan Cucurell <zarcophage@localhost.localdomain>
2022-04-10 16:20:28 -0400

    funcion binario

--- main.cpp
+++ main.cpp
@@ -21,6 +21,7 @@ void lectura_text(){
     ifstream file;
     file.open("casoT1/servicios.txt");
     if (!file.is_open()){
+        cout<<"file damaged"<<endl;
         return;
     }
     string stringLine;
@@ -45,7 +46,7 @@ void lectura_text(){
         cout<< stringLine << endl;
 
     }
-    delete[] p;
+    delete [] p;
     file.close();
 };
 
@@ -78,8 +79,27 @@ void translate_binary(){
     textFile.close();
 }
 
-int main(){
-    translate_binary();
+void leer_bin(){
+    ifstream file;
+    file.open("casoT1/tickets.dat", ios::binary);
+    if(!file.is_open()){
+        cout<<"file damaged"<<endl;
+        return;
+    }
+    int num;
+    file.read((char*) &num, sizeof(int));
+    cout<<num<<endl;
+    Ticket* p = new Ticket[num];
+    while(file.read((char*) p, sizeof(Ticket)));
+    cout<< p[0].rut_funcionario<<endl;
 
+    delete [] p;
+    file.close();
+    return;
+}
+
+int main(){
+    leer_bin();
+    lectura_text();
     return 0;
 }
