19e0475ac92639bf7ff3e9459f6ee1f44866d1e9
Juan Cucurell <zarcophage@localhost.localdomain>
2022-04-18 13:05:39 -0400

    funciones comentadas a detalle

--- main.cpp
+++ main.cpp
@@ -22,6 +22,25 @@ struct persona {
     int tickets_val;
     int tickets_Total;
 };
+
+
+/*****
+*   Puntero struct datos  lectura_text
+******
+*   recupera los datos del archivo de servicios y los pasa a un arreglo de una estructura previamente declarada
+    de tipo nombre "datos", la cual permite almacenar el nombre de un servicio, su limite diario, su limite mensual y las horas de inicio y final,
+    la función lee el archivo y realiza una lectura por formato para obtener los datos y guardarlos en un arreglo dinámico con el numero rescatado
+    del archivo de texto, que posteriormente se retorna.
+
+******
+*   Input:
+*       dirección de memoria de un entero  number : permite guardar el numero que se recupera en el archivo de servicios para futuro uso
+******
+*   Return:
+*       puntero estructura datos, retorna el arreglo dinámico con los datos de los servicios que se encuentran en el archivo servicios.tx
+
+*****/
+
 datos* lectura_text(int &number){
     ifstream file;
     file.open("casoT1/servicios.txt");
@@ -58,6 +77,20 @@ datos* lectura_text(int &number){
     return p;
 };
 
+/*****
+*   Puntero struct ticket  leer_bin
+******
+*   lee el archivo binario y almacena los datos relacionados en un arreglo dinámico de tipo Ticket que posteriormente se retorna.
+    además, la función modifica el valor contenido en la dirección de memoria del parámetro que recibe y almacena el valor del entero
+    contenido en el archivo binario para posterior uso 
+******
+*   Input:
+*       dirección de memoria de un numero entero  number : permite guardar el numero entero almacenado en el archivo binario para su futuro uso
+******
+*   Returns:
+*       puntero de estructura Ticket, retorna un arreglo dinámico de la estructura Ticket, con los datos almacenados en el binario, para su posterior procesamiento
+
+*****/
 
 Ticket* leer_bin(int &number){
     ifstream file;
@@ -73,6 +106,19 @@ Ticket* leer_bin(int &number){
     return p;
 }
 
+/*****
+*   void heapy
+******
+*   construye un maxHeap, un arbol binario con propiedad de que el nodo padre/raiz sea mayor a sus nodos hijos, intercambiando posiciones en el arreglo y construyendo el heap en el proceso,
+*   cuando se produzca un cambio de posiciones en heap, también se producirá un cambio en posiciones de los arreglos de Tickets y el arreglo auxiliar,
+*   posteriormente se llama recursivamente a la función para contruir heaps y nodos hijos, al terminar de construirse el maxHeap, se ordenan los arreglos de forma correcta a través de la función heapsort
+******
+*   Input:
+*       Ticket* arregloOriginalHeap : arreglo original con los datos de los tickets emitidos en el mes, es decir, información relacionada a los datos del archivo binario
+*       int* arregloAuxHeap : arreglo de ruts acortados que funciona como arreglo de numeros enteros con las mismas posiciones del arreglo original de los Tickets para su ordenamiento y la construcción del maxHeap
+*       int tamanoArreglo : tamano del arreglo auxiliar y el arreglo original de los tickets, es decir, la cantidad total de tickets emitidos en el mes
+*       int i : iteración del ciclo y posición del arreglo con el que se llamó a la función
+*****/
 void heapy(Ticket* arregloOriginalHeap, int* arregloAuxHeap, int tamanoArreglo, int i){
     int mayorLongitud = i; 
     int izq = 2*i + 1; 
@@ -91,6 +137,18 @@ void heapy(Ticket* arregloOriginalHeap, int* arregloAuxHeap, int tamanoArreglo, 
     }
 }   
 
+/*****
+*   void heapSort
+******
+*   toma el arreglo original de los datos del binario y el arreglo auxiliar de los ruts de los funcionarios y crea un Heap para ordenar los datos presentes en los arreglos,
+*   luego extrae uno a uno los elementos del heap creado con los elementos del arreglo y realiza cambios en las posiciones del arreglo auxiliar y a la vez al arreglo de tickets, ordenando
+*   lexicográficamente el arreglo original y el auxiliar
+******
+*   Input:
+*       Ticket* arregloOriginal : arreglo relacionado a los datos recuperados del binario, con la información de los tickets
+*       int* arregloAux : arreglo auxiliar con los ruts acortados respecto al arreglo relacionado a los tickets
+*       int tamanoArreglo : tamano total del arreglo que se pretende ordenar, en este caso, la cantidad de Tickets totales emitidos en el mes
+*****/
 void heapSort(Ticket* arregloOriginal, int* arregloAux, int tamanoArreglo){
     for (int i = tamanoArreglo / 2 - 1; i >= 0; i--){ //crea un heap, reestructurando el arreglo
         heapy(arregloOriginal, arregloAux, tamanoArreglo, i);
@@ -102,6 +160,17 @@ void heapSort(Ticket* arregloOriginal, int* arregloAux, int tamanoArreglo){
         heapy(arregloOriginal, arregloAux, i, 0);
     };
 };
+
+/*****
+*   void ordenarTicketsPersona
+******
+*   crea un arreglo dinámico auxiliar con la parte entera de los ruts de cada funcionario (digitos previos al digito verificador, ya que este es unico para cada rut), y con este arreglo se llama al algoritmo de ordenamiento,
+*   asociando las posiciones de este arreglo auxiliar a las posiciones del arreglo de los datos recuperados del archivo binario, permitiendo ordenar ambos arreglos en orden lexicográfico
+******
+*   Input:
+*       Ticket* datosBinario : arreglo que contiene los datos rescatados del archivo binario, es decir, información de todos los tickets emitidos del mes
+*       int numBin : numero entero rescatado del archivo binario que indica el total de tickets emitidos en el mes    
+*****/
 
 void ordenarTicketsPersona(Ticket* datosBinario, int numBin){
 
@@ -117,6 +186,21 @@ void ordenarTicketsPersona(Ticket* datosBinario, int numBin){
 
     delete [] toSort;
 };
+
+/*****
+*   persona* personasUnicas
+******
+*   detecta cada funcionario único dentro de los datos de tickets emitidos, cuenta los funcionarios totales y registra a cada uno en un arreglo dinámico de
+*   una estructura de tipo "persona", que contiene el rut de cada funcionario y permitirá almacenar sus tickets validos y totales
+******
+*   Input:
+*       Ticket* datosBinary : arreglo con los datos rescatados del archivo binario, es decir, la información de los tickets emitidos durante el mes
+*       int numBin : numero entero rescatado del archivo binario, referente a la cantidad total de tickets emitidos en general
+*       int &cantidadPersonas : dirección de memoria de un entero previamente declarado que permitirá almacenar la cantidad total de funcionarios unicos que se detecten en los datos del archivo binario 
+*****
+*   Return:
+*       persona*, arreglo dinámico que contiene cada rut de cada funcionario único detectado en los datos de los tickets, el cual permite guardar los tickets validos y totales de cada funcionario
+*****/
 
 persona* personasUnicas(Ticket* datosBinary, int numBin, int &cantidadPersonas){
     string rutActual;
@@ -142,6 +226,19 @@ persona* personasUnicas(Ticket* datosBinary, int numBin, int &cantidadPersonas){
     return personas;
 }
 
+/*****
+*   void TicketsTotalesPersonas
+******
+*   proceso que cuenta los tickets totales emitidos por cada funcionario único y almacena aquello en la casilla de "tickets totales" de una posición de un arreglo previamente declarado de tipo "personas", modificando
+*   aquel arreglo, el cual contendrá los datos de cada persona (rut, tickets válidos y tickets totales) para el retorno final del programa
+******
+*   Input:
+*       persona* personasUnicas : arreglo que contiene datos de cada funcionario unico existente detectado previamente entre los datos de los tickets emitidos
+*       Ticket* datosBinario : arreglo que contiene los datos recuperados en el archivo binario tickets.dat, el cual contiene información acerca de todos los tickets emitidos
+*       int cantidadPersonas : cantidad de funcionarios unicos detectados previamente en el archivo binario con con la informacion de los tickets emitidos
+*       int numBin : cantidad total de tickets emitidos, numero entero recuperado del archivo binario con la información de los tickets emitidos
+*****/
+
 void TicketsTotalesPersonas (persona* personasUnicas, Ticket* datosBinario, int cantidadPersonas, int numBin){
     string rutActual, rutAContar;
     int contadorTicketsPersona;
@@ -158,6 +255,23 @@ void TicketsTotalesPersonas (persona* personasUnicas, Ticket* datosBinario, int 
     }
 
 }
+
+/*****
+*   bool VerificiarValHora
+******
+*   permite verificar la validez de un ticket respecto a las horas de un servicio, verificando si el ticket fue emitido entre el rango de horas de un servicio
+******
+*   Input:
+*       int horaTicket : hora en la que fue emitido un ticket
+*       int minTicket : minuto de cierta hora en que fue emitido un ticket
+*       int horaInicioServ : hora de inicio de cierto servicio que se haya conseguido en el archivo Servicios.txt
+*       int minInicioServ : minuto de cierta hora en la que inicia el servicio
+*       int horaFinServ : hora de fin de cierto servicio que se haya conseguido en el archivo Servicios.txt
+*       int minFinServ : minuto de la hora en que finaliza el servicio
+******
+*   Return:
+*       bool, retorna un valor booleano referente a la validez de un ticket, si el ticket es válido con respecto a las horas de un servicio, retorna un valor verdadero, si el ticket fue emitido fuera de hora, retorna un valor falso
+*****/
 
 bool verificarValHora(int horaTicket, int minTicket, int horaInicioServ, int minInicioServ, int horaFinServ, int minFinServ){
     if (horaTicket == horaInicioServ){ //si horas son iguales, comparamos minutos
@@ -177,7 +291,20 @@ bool verificarValHora(int horaTicket, int minTicket, int horaInicioServ, int min
     }
     return false;
 }
-
+/*****
+*   void ticketsValidos
+******
+*   proceso que establece y cuenta los tickets validos de una persona respecto a las horas en que fue emitido, valida si la cantidad de tickets emitidos por una persona supera el limite diario de un servicio
+*   o si supera el limite mensual del mismo servicio, y posteriormente guarda esta información en el arreglo que contiene los datos de cada funcionario único, sus tickets totales y sus tickets válidos
+******
+*   Input:
+*       persona* personasUnicas : arreglo que contiene la información (rut, tickets totales y tickets validos) de cada funcionario único detectado en el archivo binario
+*       Ticket* datosBinario : arreglo que contiene los datos recuperados del archivo binario, es decir, todos los tickets emitidos durante el mes
+*       datos* datosServicios : arreglo que contiene la información recuperada del archivo de texto, es decir, los datos acerca de cada cada servicio (nombre, limite diario de tickets, limite mensual de tickets y rango horario)
+*       int cantidadPersonas : cantidad de funcionarios/ruts únicos detectados en los tickets emitidos durante el mes
+*       int numBin : numero total de tickets emitidos durante el mes, numero entero rescatado del archivo binario
+*       int numTxt : numero total de servicios que se ofrecen, numero entero rescatado del archivo de texto        
+*****/
 void ticketsValidos (persona* personasUnicas, Ticket* datosBinario, datos* datosServicios, int cantidadPersonas, int numBin, int numTxt){
     //cout<<"----FUNCION TICKETS VALIDOS----"<<endl;
     int horaTicket, minTicket, horaInicioServ, minInicioServ, horaFinServ, minFinServ, contadorTicketsVal, contLimMens, limMensual, contLimDiario, limDiario, contInval;
@@ -295,14 +422,15 @@ void ticketsValidos (persona* personasUnicas, Ticket* datosBinario, datos* datos
         }
     }
 
-    for (int i = 0; i<cantidadPersonas; i++){
+/*    for (int i = 0; i<cantidadPersonas; i++){
         cout<<personasUnicas[i].rut<<endl;
         cout<<personasUnicas[i].tickets_val<<endl;
         cout<<personasUnicas[i].tickets_Total<<endl;
     }
 
-
+*/
     cout<<"----FIN FUNCION TICKETS VALIDOS----"<<endl;
+
 }
 
 int main(){
